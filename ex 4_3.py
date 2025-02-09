c0ca1d870a99ee044ab4b859dbaa8139fcfb0363
elenaproulova <elenkaf2233@gmail.com>
2025-02-09 17:58:44 +0500

    add code  ok

---
+++ ex 4_3.py
@@ -0,0 +1,30 @@
+import time
+import csv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Chrome()
+url = "https://www.divan.ru/ekaterinburg/category/divany"
+driver.get(url)
+time.sleep(3)
+
+prices = driver.find_elements(By.CLASS_NAME, "ui-LD-ZU")
+
+parsed_data = []
+# Открытие CSV файла для записи
+with open('prices.csv', mode='w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Price'])  # Записываем заголовок столбца
+
+    # Записываем цены в CSV файл
+    for price in prices:
+        writer.writerow([price.text])
+
+# Закрытие драйвера
+driver.quit()
+
+with open("divan_price.csv", 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Цена'])
+    writer.writerows(parsed_data)
